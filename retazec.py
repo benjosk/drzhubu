1f9b15864b32d8a6e8d13aca9b7a206ff13ae2c0
benjosk <91186933+benjosk@users.noreply.github.com>
2021-09-22 11:26:43 +0200

    Add files via upload

--- retazec.py
+++ retazec.py
@@ -12,8 +12,21 @@
 # print(f"*  {meno}  *")
 # print("*" * (len(meno)+6))
 
-meno = input("meno:")
-for i in range (len(meno)):
-    print(meno[i] * (i+1))
+# meno = input("meno:")
+# for i in range (len(meno)):
+#     print(meno[i] * (i+1))
 
+def sekaj(text, cislo):
+    i = 0
+    while i <= len(text):
+        i += cislo
+        text = text[:i] + "\n" + text[i:]
+        i += 1
+    return(text)
+
+x = int(input("cislo:"))
+hym = "Nad Tatrou sa blýska, hromy divo bijú, zastavme ich, bratia, veď sa ony stratia, Slováci ožijú."
+
+
+print(sekaj(hym,x))
 
