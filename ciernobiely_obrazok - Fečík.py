bad4f600de414cdec574d5244a27ab04502388c6
benjosk <91186933+benjosk@users.noreply.github.com>
2021-10-15 10:25:37 +0200

    Add files via upload

--- ciernobiely_obrazok - Fečík.py
+++ ciernobiely_obrazok - Fečík.py
@@ -1,25 +1,31 @@
 import tkinter
+import random
 
 subor = open("sen.txt", "r")
-text = subor.read().split()
+text = subor.read().splitlines()
+rozmery = str(text[0]).split()
+sirka, vyska = int(rozmery[0]), int(rozmery[1])
+text2 = text[1:]
+print(text2)
+znaky = []
+for j in text2:
+    for i in range(0, len(text)-1, 2):
+        znaky.append(j[i] + j[i+1])
 
-canvas = tkinter.Canvas(width=text[0], height=text[1], bg="white")
-canvas.pack()
+print(znaky)
 
-farby = list(text[2])
-print(farby)
 
-def mozaika():
-    x,y = 1,1
+def mozaika(sirka_platna, vyska_platna):
+    x,y = 0,0
     a = 10
-    i = 1
-    while y < int(text[1]):
-        while x < int(text[0]):
-            canvas.create_rectangle(x,y,x+a,y+a)
-            i += 1
+    while y < vyska_platna + a:
+        while x < sirka_platna +a:
+            farba = f"#{random.randrange(256):02x}{random.randrange(256):02x}{random.randrange(256):02x}"
+            canvas.create_rectangle(x,y,x+a,y+a, fill=farba, outline=farba)
             x += a
-        x = 1
+        x = 0
         y += a
 
-mozaika()
+canvas = tkinter.Canvas(width=sirka, height=vyska, bg="white")
+canvas.pack()
 canvas.mainloop()
